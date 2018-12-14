97d0c0db8dde63d55ff0c834d49045e40131d3d5
owner <owner@breath>
2018-12-14 16:02:52 +0100

    initial commit

---
+++ src/ui/CLI.java
@@ -0,0 +1,13 @@
+package ui;
+
+public class CLI
+{
+    public static void main(String[] args)
+    {
+        int selection = 0;
+        
+        do{
+            
+        }while(selection != 99);
+    }
+}
