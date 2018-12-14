f620dc43b3d504869e82628032a36374529b0ea4
owner <owner@breath>
2018-12-14 17:47:51 +0100

    Hasher is implemented

--- src/security/Hasher.java
+++ src/security/Hasher.java
@@ -1,17 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package security;
 
-/**
- *
- * @author Andreas
- */
-public class Hasher {
-    
-    public static void hash(){
-        
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public final class Hasher
+{
+    private final static char [] hexAlphabet = "0123456789ABCDEF".toCharArray();
+
+    public static String hash(String password)
+            throws NoSuchAlgorithmException
+    {
+        MessageDigest messageDig = MessageDigest.getInstance("MD5");
+        messageDig.update(password.getBytes());
+        byte[] digest = messageDig.digest();
+        return bytesToHex(digest);
     }
+
+    public static String bytesToHex(byte[] bytes)
+    {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexAlphabet[v >>> 4];
+            hexChars[j * 2 + 1] = hexAlphabet[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
 }
